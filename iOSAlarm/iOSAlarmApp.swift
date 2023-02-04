eecc3eba8c137b9a12eb3c4877e56e92ca69e954
VirajTank <virajtank98@gmail.com>
2023-02-04 21:55:41 +0530

    Initial Commit

---
+++ iOSAlarm/iOSAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAlarmApp.swift
+//  iOSAlarm
+//
+//  Created by Viraj Tank on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
